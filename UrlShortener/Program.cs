92f84adf67da2601e165f170c4e42b184703d03f
Oybek Kuchkarov <oybekkuch@gmail.com>
2023-09-03 13:17:42 +0500

    s

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -4,12 +4,14 @@ using UrlShortener.Entities;
 using UrlShortener.Extensions;
 using UrlShortener.Models;
 using UrlShortener.Services;
+using Microsoft.Extensions.Caching.Memory;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddMemoryCache();
 builder.Services.AddDbContext<ApplicationDbContext>(s =>
     s.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
 
@@ -33,7 +35,8 @@ if (app.Environment.IsDevelopment())
 app.MapPost("api/shorten", async (ShortenUrlRequest request,
         UrlShorteningService shorteningService,
         ApplicationDbContext applicationDbContext,
-        HttpContext httpContext) =>
+        HttpContext httpContext,
+        IMemoryCache cache) =>
 {
     if (!Uri.TryCreate(request.Url, UriKind.Absolute, out _))
     {
@@ -51,6 +54,10 @@ app.MapPost("api/shorten", async (ShortenUrlRequest request,
         CreatedOnUtc = DateTime.UtcNow
     };
 
+    var key = $"{shortenedUrl.Code}";
+    cache.Set(key, shortenedUrl, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(2)));
+
+
     await applicationDbContext.ShortenedUrls.AddAsync(shortenedUrl);
 
     await applicationDbContext.SaveChangesAsync();
@@ -61,12 +68,22 @@ app.MapPost("api/shorten", async (ShortenUrlRequest request,
 
 
 app.MapGet("api/{code}", async (string code,
-    ApplicationDbContext dbContext) =>
+    ApplicationDbContext dbContext, 
+    IMemoryCache cache) =>
 {
-    var shortenedUrl = await dbContext.ShortenedUrls
-        .FirstOrDefaultAsync(s => s.Code == code);
+    cache.TryGetValue(code, out ShortenedUrl? url);
+    if (url is not null) return Results.Redirect(url.LongUrl);
+    var shortenedUrl = await dbContext.ShortenedUrls.FirstOrDefaultAsync(s => s.Code == code);
 
-    return shortenedUrl is null ? Results.NotFound() : Results.Redirect(shortenedUrl.LongUrl);
+    if (shortenedUrl is null)
+    {
+        return Results.NotFound();
+    }
+
+    cache.Set(code, shortenedUrl, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(2)));
+
+    return Results.Redirect(shortenedUrl.LongUrl);
+
 });
 
 app.UseHttpsRedirection();
